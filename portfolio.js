fca31613f513f16e48d5f18a3959e68df35e8f09
cgad <35946778+cgad@users.noreply.github.com>
2019-01-04 18:00:40 -0700

    Reformat code with prettify, change node app demo video links

--- portfolio.js
+++ portfolio.js
@@ -1,83 +1,131 @@
 $(".carbon").on("click", function() {
   $("#appName").html("<strong>Carbon Neutral</strong>");
-  $("#site").html("<p><a href='https://cryptic-woodland-29160.herokuapp.com/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/carbon-neutral target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cryptic-woodland-29160.herokuapp.com/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/carbon-neutral target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".onion").on("click", function() {
   $("#appName").html("<strong>Onion Scraper</strong>");
-  $("#site").html("<p><a href='https://aqueous-ridge-77825.herokuapp.com/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/onion-scraper target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://aqueous-ridge-77825.herokuapp.com/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/onion-scraper target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".isLegit").on("click", function() {
   $("#appName").html("<strong>Is This Legit?</strong>");
-  $("#site").html("<p><a href='https://peaceful-savannah-96764.herokuapp.com/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/Jaco5/project-two-new target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://peaceful-savannah-96764.herokuapp.com/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/Jaco5/project-two-new target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".friend").on("click", function() {
   $("#appName").html("<strong>Friend Finder</strong>");
-  $("#site").html("<p><a href='https://glacial-lowlands-57080.herokuapp.com/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/FriendFinder target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://glacial-lowlands-57080.herokuapp.com/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/FriendFinder target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".train").on("click", function() {
   $("#appName").html("<strong>Train Scheduler</strong>");
-  $("#site").html("<p><a href='https://cgad.github.io/train-scheduler/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/train-scheduler target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cgad.github.io/train-scheduler/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/train-scheduler target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".liri").on("click", function() {
   $("#appName").html("<strong>Liri Bot</strong>");
-  $("#site").html("<p><a href='https://drive.google.com/file/d/1Jp3npdVc21HpZZdAPoF76RAqTvUifqul/view target=_blank'>Node App Demo Video</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/liri-node-app target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://drive.google.com/file/d/1AYgZHSsnMJQxh6BsOTEs94EWBGTBCWyb/view?usp=sharing target=_blank'>Node App Demo Video</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/liri-node-app target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".bamazon").on("click", function() {
   $("#appName").html("<strong>Bamazon</strong>");
-  $("#site").html("<p><a href='https://drive.google.com/file/d/12vv5lFUAWZRk6yO9kqhbOQdZZs7lhj5A/view target=_blank'>Node App Demo Video</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/bamazon target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://drive.google.com/file/d/12vv5lFUAWZRk6yO9kqhbOQdZZs7lhj5A/view?usp=sharing target=_blank'>Node App Demo Video</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/bamazon target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".giphy").on("click", function() {
   $("#appName").html("<strong>GifTastic</strong>");
-  $("#site").html("<p><a href='https://cgad.github.io/giftastic/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/giftastic target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cgad.github.io/giftastic/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/giftastic target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".crystals").on("click", function() {
   $("#appName").html("<strong>Crystals Collector</strong>");
-  $("#site").html("<p><a href='https://cgad.github.io/unit-4-game/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/unit-4-game target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cgad.github.io/unit-4-game/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/unit-4-game target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".burger").on("click", function() {
   $("#appName").html("<strong>Eat-Da-Burger</strong>");
-  $("#site").html("<p><a href='https://mighty-tor-46071.herokuapp.com/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/burger target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://mighty-tor-46071.herokuapp.com/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/burger target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".disney").on("click", function() {
   $("#appName").html("<strong>Disney Trivia</strong>");
-  $("#site").html("<p><a href='https://cgad.github.io/TriviaGame/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/TriviaGame target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cgad.github.io/TriviaGame/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/TriviaGame target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
 
 $(".fruit").on("click", function() {
   $("#appName").html("<strong>Guess My Fruit</strong>");
-  $("#site").html("<p><a href='https://cgad.github.io/Word-Guess-Game/ target=_blank'>Web App</a></p>");
-  $("#github").html("<p><a href='https://github.com/cgad/Word-Guess-Game target=_blank'>GitHub Repo</a></p>");
+  $("#site").html(
+    "<p><a href='https://cgad.github.io/Word-Guess-Game/ target=_blank'>Web App</a></p>"
+  );
+  $("#github").html(
+    "<p><a href='https://github.com/cgad/Word-Guess-Game target=_blank'>GitHub Repo</a></p>"
+  );
   $(".modal").modal("show");
 });
