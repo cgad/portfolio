8279328ec05b60e53ceef00140efc91e31234381
cgad <35946778+cgad@users.noreply.github.com>
2019-03-21 21:01:29 -0600

    Begin redesign

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
-$(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 3,
-        nav: false
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: false
-      }
-    }
-  });
-});
+window.onscroll = function() {
+  if (
+    document.body.scrollTop > 160 ||
+    document.documentElement.scrollTop > 160
+  ) {
+    document.getElementById("header").className = "header";
+    document.getElementById("icons").innerHTML = "";
+  } else {
+    document.getElementById("header").className = "";
+    document.getElementById("icons").innerHTML =
+      '<img class="github" src="src/assets/images/GitHub-Mark-Light-64px.png" alt="Link to GitHub"/> <img class="email" src="src/assets/images/email-white.png" alt="Link to E-Mail"/><img src="src/assets/images/linkedin-white.png" alt="Link to LinkedIn" class="linkedin"/>';
+  }
+};
